f499055f0a210221f4a87e8341f61e593b49a0ec
ilyakravec <iliakraff@gmail.com>
2023-12-07 16:11:24 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// modal 
+
+$(document).ready(function() {
+    $('#myTable').DataTable();
+});
+
+const modalCall = $("[data-modal]");
+const modalClose = $("[data-close]");
+
+modalCall.on("click", function(event){
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('show');
+
+})
+
+modalClose.on("click", function(event){
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.modal');
+
+    modalParent.removeClass('show');
+})
+
